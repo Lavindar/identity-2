61b9657d943ca9d68c8e596c6e3e03ec4fd31169
Nicolas Ward <ultranurd@yahoo.com>
2012-11-01 23:11:40 -0400

    bump the TOC after testing in Pandaria; everything checks out with no changes

--- Identity-2.lua
+++ Identity-2.lua
@@ -10,7 +10,7 @@
 -----
 
 -- Sets the current Identity version
-local Identity_VERSION = "2.1-40000";
+local Identity_VERSION = "2.1-50001";
 
 -- Stores the unmodified chat message
 local Identity_OriginalSendChatMessage;
